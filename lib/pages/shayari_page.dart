e8b6c214cdeabd6e632ff815e6b73e07c19ed33a
Pradeep <pawarpradeep1707@gmail.com>
2025-02-19 11:08:59 +0530

    Weve created a home page and a shayari list page

---
+++ lib/pages/shayari_page.dart
@@ -0,0 +1,17 @@
+final List<Map<String, String>> shayariList = [
+  {
+    "category": "Love",
+    "text": "Mohabbat bhi ajeeb cheez hai, Dil ko tod bhi deti hai aur jodne bhi aati hai.",
+    "color": "#FF4A4A"
+  },
+  {
+    "category": "Sad",
+    "text": "Zindagi ne diye jo gham, unhe sajana seekh liya...",
+    "color": "#3A3A3A"
+  },
+  {
+    "category": "Motivation",
+    "text": "Safalta ki chabi hai, kabhi haar na maanna!",
+    "color": "#4CAF50"
+  }
+];
