4139d9c93394e563d2618b0fe1ee779c49dda2be
Pradeep <pawarpradeep1707@gmail.com>
2025-02-19 11:23:21 +0530

    Now will add some background images and audio

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:shayari/pages/shayari_page.dart';
+// import 'shayari_data.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final pages = shayariList
+        .map((shayari) => Container(
+              color: Color(int.parse("0xFF${shayari['color']!.substring(1)}")),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        shayari['text']!,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontFamily: 'GoogleFonts.lobster().fontFamily',
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ))
+        .toList();
+
+    return Scaffold(
+      body: LiquidSwipe(
+        pages: pages,
+        enableLoop: true,
+        waveType: WaveType.liquidReveal,
+        slideIconWidget: Icon(Icons.arrow_back_ios, color: Colors.white),
+      ),
+    );
   }
 }
